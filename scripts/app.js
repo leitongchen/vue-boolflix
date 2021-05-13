4c4152167b86864c5ae52661b14a454ef4a8b830
leitong <leitongchen93@gmail.com>
2021-05-13 16:21:06 +0200

    Milestone 1 completato

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,58 @@
 const app = new Vue({
-  
-    el: "#app", 
+
+    el: "#app",
     data: {
 
+        tmdbAPIKey: "ad3dc44159bbda29e23cab3d107aa841",
+        userSearch: "",
 
-    }, 
+        moviesList: [],
+
+    },
     methods: {
+        onSearchClick() {
 
+            const axiosParam = {
+                params: {
+                    api_key: this.tmdbAPIKey,
+                    query: this.userSearch,
+                    language: "it-IT"
+                }
+            };
 
-    } 
+            axios.get("https://api.themoviedb.org/3/search/movie", axiosParam).then((resp) => {
+
+                /*
+                Key da stampare: 
+                - title
+                - original_title
+                - original_language
+                - vote_average
+                */
+
+                this.moviesList = [...resp.data.results]
+
+                console.log(this.moviesList)
+
+                
+            })
+
+        }
+
+    },
+    mounted() {
+        // axios.get("https://api.themoviedb.org/3/search/movie", {
+
+        //     params: {
+        //         api_key: this.tmdbAPIKey,
+        //         query: this.userSearch,
+        //         language: "it-IT"
+        //     }
+
+        // }).then((resp) => {
+        //     console.log("ciao");
+        //     console.log(resp);
+        // });
+
+    }
 })
